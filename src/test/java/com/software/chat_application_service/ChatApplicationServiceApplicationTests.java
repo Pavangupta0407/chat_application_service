2b39678f00f4f5fc90dd93837d64bd2c2b0f5492
PAVAN_ACER\Pavan <pavangupta0407@gmail.com>
2024-12-09 15:31:14 +0530

    Initial commit

---
+++ src/test/java/com/software/chat_application_service/ChatApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.chat_application_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
